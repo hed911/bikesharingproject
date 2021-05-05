2b2154628a2035dff5bc7bea03d959ab135f1b89
Eduardo Escobar <eduardoescobar@Eduardos-MacBook-Pro.local>
2021-05-05 08:03:32 -0500

    Created the sectors graphics

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const dimensions = {
 // make that the draw one only specify what fields take to draw 
 const drawScatterPlot = async (id, xData, yData) => {
   // Data
-  const dataset = await d3.json('./data/data.json');
+  const dataset = await d3.json('./data/general.json');
   const xAccessor = (d) => d[xData.attribute];
   const yAccessor = (d) => d[yData.attribute];
 
@@ -68,7 +68,7 @@ const drawScatterPlot = async (id, xData, yData) => {
 
   xAxisGroup.append('text')
     .attr('x', dimensions.ctrWidth / 2)
-    .attr('y', dimensions.margin.bottom - 90)
+    .attr('y', dimensions.margin.bottom - 0)
     .attr('fill', 'black')
     .text(xData.attributeName);
 
@@ -80,11 +80,95 @@ const drawScatterPlot = async (id, xData, yData) => {
 
   yAxisGroup.append('text')
     .attr('x', -dimensions.ctrHeight / 2)
-    .attr('y', -dimensions.margin.left + 70)
+    .attr('y', -dimensions.margin.left + 10)
     .attr('fill', 'black')
     .html(yData.attributeName)
     .style('transform', 'rotate(270deg)')
     .style('text-anchor', 'middle');
+}
+
+const drawSectors = async (id, datasetFilename) => {
+  // Data
+  const dataset = await d3.json(`./data/${datasetFilename}.json`);
+
+  // Dimensions
+  let dimensions = {
+    width: 600,
+    height: 600,
+    margins: 10,
+  }
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+  const radius = dimensions.ctrWidth / 2
+
+  // Draw Image
+  const svg = d3.select(id)
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height)
+
+  const ctr = svg.append("g") // <g>
+    .attr(
+      "transform",
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    )
+
+  // Scales
+  const populationPie = d3.pie()
+    .value((d) => d.value)
+    .sort(null)
+  const slices = populationPie(dataset)
+
+  const arc = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(0)
+  const arcLabels = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(200)
+
+  const colors = d3.quantize(d3.interpolateSpectral, dataset.length)
+  const colorScale = d3.scaleOrdinal()
+    .domain(dataset.map(element => element.name))
+    .range(colors)
+
+  // Draw Shape
+  const arcGroup = ctr.append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
+    )
+
+  arcGroup.selectAll('path')
+    .data(slices)
+    .join('path')
+    .attr('d', arc)
+    .attr('fill', d => colorScale(d.data.name))
+
+  const labelsGroup = ctr.append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
+    )
+    .classed('labels', true)
+
+  labelsGroup.selectAll('text')
+    .data(slices)
+    .join('text')
+    .attr('transform', d => `translate(${arcLabels.centroid(d)})`)
+    .call(
+      text => text.append('tspan')
+        .style('font-weight', 'bold')
+        .attr('y', -4)
+        .text(d => d.data.name)
+    )
+    .call(
+      text => text.filter((d) => (d.endAngle - d.startAngle) > 0.25)
+        .append('tspan')
+        .attr('y', 9)
+        .attr('x', 0)
+        .text(d => d.data.value)
+    )
 }
 
 drawScatterPlot('#chart-one',
@@ -98,3 +182,9 @@ drawScatterPlot('#chart-two',
 drawScatterPlot('#chart-three',
   { attribute: 'windspeed', attributeName: 'Velocidad del viento' },
   { attribute: 'cnt', attributeName: 'Cantidad de Viajes' });
+
+[
+  ['four', 'weathers'],
+  ['five', 'seasons'],
+  ['six', 'laboral_days']
+].forEach(e => drawSectors(`#chart-${e[0]}`, e[1]));
